e51dc82ed2109962e46743a50a2d632744ad41df
Kevin Zeng <kz571@nyu.edu>
2016-11-08 14:09:10 -0500

    Delegated Message to send

--- app/src/main/java/com/toybox/quickdraw/MainActivity.java
+++ app/src/main/java/com/toybox/quickdraw/MainActivity.java
@@ -1,20 +1,16 @@
 package com.toybox.quickdraw;
 
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.NfcEvent;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,25 +18,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 import static android.nfc.NdefRecord.createMime;
 
-public class MainActivity extends AppCompatActivity
-        implements  NfcAdapter.OnNdefPushCompleteCallback,
-        NfcAdapter.CreateNdefMessageCallback {
+public class MainActivity extends AppCompatActivity {
     NfcAdapter nfcAdpt;
     PendingIntent pendingIntent;
     IntentFilter writeTagFilters[];
-    Context thisContext;
-    boolean writeMode;
-    Tag mytag;
     TextView tv;
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+    }
+
+    //Write Phone code here
+    String writeMessage() {
+        String message = "Hello World";
+        return message;
     }
 
     @Override
@@ -50,12 +46,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        thisContext = this;
+        tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Button Pressed", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                tv.setText("");
             }
         });
 
@@ -80,88 +78,17 @@ public class MainActivity extends AppCompatActivity
         tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
         writeTagFilters = new IntentFilter[] {tagDetected};
 
-        // Register callback
-        nfcAdpt.setNdefPushMessageCallback(this, this);
-        nfcAdpt.setOnNdefPushCompleteCallback(this, this);
-
-        // Example of a call to a native method
-        tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        //Set additional messages
+        SetMessage(writeMessage());
     }
 
-    @Override
-    public NdefMessage createNdefMessage(NfcEvent event) {
-        tv.setText(tv.getText() + "\nCreate Ndef Message: " + event.toString());
-        String text = ("Beam me up, Android!\n\n" + "Beam Time: " + System.currentTimeMillis());
+    void SetMessage(String text) {
+        //append more ndefrecords
         NdefMessage msg = new NdefMessage(
-                new NdefRecord[] { createMime(
-                        "text/plain", text.getBytes())
-                        /**
-                         * The Android Application Record (AAR) is commented out. When a device
-                         * receives a push with an AAR in it, the application specified in the AAR
-                         * is guaranteed to run. The AAR overrides the tag dispatch system.
-                         * You can add it back in to guarantee that this
-                         * activity starts when receiving a beamed message. For now, this code
-                         * uses the tag dispatch system.
-                        */
-                        //,NdefRecord.createApplicationRecord("com.example.android.beam")
+                new NdefRecord[] {
+                        createMime("text/plain", text.getBytes())
                 });
-        return msg;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        tv.setText(tv.getText() + "\nResume Intent: " + getIntent().getAction());
-        // Check to see that the Activity started due to an Android Beam
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            processIntent(getIntent());
-        }
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        tv.setText(tv.getText() + "\nFound Intent: " + intent.getAction());
-        if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())){
-            mytag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            tv.setText(tv.getText() + "\nTag detected: " + mytag.toString());
-
-            Toast.makeText(this, "Tag detected", Toast.LENGTH_LONG).show();
-            Snackbar.make(findViewById(R.id.content_main), "Tag detected", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-        }
-    }
-
-    void processIntent(Intent intent) {
-        tv.setText(tv.getText() + "\nProcessing Intent: " + intent.getAction());
-    }
-
-    private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
-
-        //create the message in according with the standard
-        String lang = "en";
-        byte[] textBytes = text.getBytes();
-        byte[] langBytes = lang.getBytes("US-ASCII");
-        int langLength = langBytes.length;
-        int textLength = textBytes.length;
-
-        byte[] payload = new byte[1 + langLength + textLength];
-        payload[0] = (byte) langLength;
-
-        // copy langbytes and textbytes into payload
-        System.arraycopy(langBytes, 0, payload, 1, langLength);
-        System.arraycopy(textBytes, 0, payload, 1 + langLength, textLength);
-
-        NdefRecord recordNFC = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload);
-        return recordNFC;
-    }
-
-    private void write(String text, Tag tag) throws IOException, FormatException {
-        NdefRecord[] records = { createRecord(text) };
-        NdefMessage message = new NdefMessage(records);
-        Ndef ndef = Ndef.get(tag);
-        ndef.connect();
-        ndef.writeNdefMessage(message);
-        ndef.close();
+        nfcAdpt.setNdefPushMessage(msg, this);
     }
 
     @Override
@@ -191,9 +118,4 @@ public class MainActivity extends AppCompatActivity
      * which is packaged with this application.
      */
     public native String stringFromJNI();
-
-    @Override
-    public void onNdefPushComplete(NfcEvent nfcEvent) {
-        tv.setText(tv.getText() + "\nPush Complete: " + nfcEvent.toString());
-    }
 }
